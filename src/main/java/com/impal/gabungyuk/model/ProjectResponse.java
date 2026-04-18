035deb09372fdf7c124c0ab7b80999cc64aee8b5
Pangeran Decaesario <pangeranclev@gmail.com>
2026-04-18 22:41:56 +0700

    feat: Create endpoint project

---
+++ src/main/java/com/impal/gabungyuk/model/ProjectResponse.java
@@ -0,0 +1,18 @@
+package com.impal.gabungyuk.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjectResponse {
+    private Integer id; 
+    private String title;
+    private String description;
+    private String repositoryLink;      
+    
+}
